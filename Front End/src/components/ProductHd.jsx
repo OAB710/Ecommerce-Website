f5e191fdefdb5cf07f0f1fbf26f9764b645ba511
niamipek <nguyentrungduc.forwork@gmail.com>
2024-11-06 19:23:38 +0700

    3:04:24

--- Front End/src/components/ProductHd.jsx
+++ Front End/src/components/ProductHd.jsx
@@ -3,7 +3,7 @@ import {TbArrowRight} from "react-icons/tb"
 const ProductHd = (props) => {
     const {product}=props;
   return (
-    <div>
+    <div className="flex items-center flex-wrap gap-x-2 medium-16 my-4 capitalize">
       Home <TbArrowRight/> Shop <TbArrowRight/> {product.category} 
       <TbArrowRight/>{product.name}
     </div>
