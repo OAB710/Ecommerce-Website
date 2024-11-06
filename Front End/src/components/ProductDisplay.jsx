8aa1ef2b075c7982178545b5c3a0b0dd4a786f04
niamipek <161831080+niamipek@users.noreply.github.com>
2024-11-06 12:05:14 +0700

    2:46:21

---
+++ Front End/src/components/ProductDisplay.jsx
@@ -0,0 +1,25 @@
+import product_rt_1 from "../assets/product_rt_1.png"
+import product_rt_2 from "../assets/product_rt_2.png"
+import product_rt_3 from "../assets/product_rt_3.png"
+import product_rt_4 from "../assets/product_rt_4.png"
+
+const ProductDisplay = (props) => {
+    const product=props;
+  return (
+    <section>
+        <div>
+            <div>
+                <img src={product_rt_1} alt="productImg" className='max-h-[99px]' />
+                <img src={product_rt_2} alt="productImg" className='max-h-[99px]' />
+                <img src={product_rt_3} alt="productImg" className='max-h-[99px]' />
+                <img src={product_rt_4} alt="productImg" className='max-h-[99px]' />
+            </div>
+            <div>
+                <img src={product.image} alt="" />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default ProductDisplay
